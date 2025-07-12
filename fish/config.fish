caa6272dd01cc826357f802b239d171cd88baad9
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-07-12 11:03:50 +0530

    Add configuration files for fish shell

---
+++ fish/config.fish
@@ -0,0 +1,42 @@
+fastfetch
+
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+
+end
+
+starship init fish | source
+if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+end
+
+# Helpful aliases
+alias ls='eza --icons=auto' # short list
+alias la='eza -a --icons=auto' # list all
+alias ll='eza -lh --icons=auto' # long list
+alias lla='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias lt='eza --icons=auto --tree' # list folder as tree
+alias pamcan='pacman'
+alias cls='clear'
+
+# Directory navigation shortcuts
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+    
+
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
